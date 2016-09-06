81de073565963c2942c2fe622abd11dfeabc33f8
Gregory Berns-Leone <gberns.l@gmail.com>
2016-09-06 08:30:07 -0500

    Almost working

--- app/controllers/bills_controller.rb
+++ app/controllers/bills_controller.rb
@@ -1,11 +1,19 @@
 class BillsController < ApplicationController
   def show
     @bill = Bill.find(params[:bill_id])
-    @user_id = params[:user_id]
+    @user_id = params[:id]
+    p @user_id
   end
 
   def update
-    "update"
+    if params[:bill][:items]
+      params[:bill][:items].each do |item_id, user_id_hash|
+        item = Item.find(item_id)
+        p user_id_hash[:user_id]
+        item.user_id = user_id_hash[:user_id][:value]
+        item.save
+      end
+    end
   end
 
   def create
@@ -16,5 +24,9 @@ class BillsController < ApplicationController
 
   def bill_params
     params.require(:bill).permit(:items, :initiator_id)
+  end
+
+  def item_params
+    params.permit(:user_id)
   end
 end
